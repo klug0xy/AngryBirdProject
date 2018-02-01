99df243c5b6e09aed861667060cd7df4f5d94ebe
bouhlel <bouhlel@bouhhich.home>
2018-02-01 23:45:44 +0100

    Obeservator for class Object when the bird touch the blackHole so we observe the velcity of the bird.

--- AngryBirdProject/src/main/Objectt.java
+++ AngryBirdProject/src/main/Objectt.java
@@ -2,48 +2,60 @@ package main;
 
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.util.Observable;
 
-public abstract class Objectt {
-	
+public abstract class Objectt extends Observable {
+
 	private double x;
 	private double y;
 	private double velocityX;
 	private double velocityY;
 	private Image icon;
-	
+
 	public double getX() {
 		return x;
 	}
+
 	public void setX(double x) {
 		this.x = x;
 	}
+
 	public double getY() {
 		return y;
 	}
+
 	public void setY(double y) {
 		this.y = y;
 	}
+
 	public Image getIcon() {
 		return icon;
 	}
+
 	public void setIcon(String iconPath) {
-		
+
 		Toolkit kit = Toolkit.getDefaultToolkit();
-		
+
 		icon = kit.getImage(iconPath);
-		//icon = icon.getScaledInstance(128, 128, Image.SCALE_SMOOTH);
-		
+		// icon = icon.getScaledInstance(128, 128, Image.SCALE_SMOOTH);
+
 		this.icon = icon;
 	}
+
 	public double getVelocityX() {
 		return velocityX;
 	}
+
 	public void setVelocityX(double velocityX) {
 		this.velocityX = velocityX;
+		setChanged();
+		notifyObservers();
 	}
+
 	public double getVelocityY() {
 		return velocityY;
 	}
+
 	public void setVelocityY(double velocityY) {
 		this.velocityY = velocityY;
 	}
